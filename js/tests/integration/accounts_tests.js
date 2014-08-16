751470e23b4b4f5d87801e925c4d1786a51e6489
toran billups <toranb@gmail.com>
2014-08-16 17:52:59 -0500

    simplified the accounts test

--- js/tests/integration/accounts_tests.js
+++ js/tests/integration/accounts_tests.js
@@ -12,13 +12,10 @@ module('accounts integration tests', {
 });
 
 test('model will populate the controller when promise resolves with 2 accounts', function() {
-    //start at login, click, get redirected in that route and 2x promises
     var accounts = [{name: "x"}, {name: "y"}];
     visit("/accounts")
     .httpRespond("get", "/api/accounts", accounts)
     .then(function() {
         missing("table tr");
-        // var rows = find("table tr").length;
-        // equal(rows, 2, rows);
     });
 });
