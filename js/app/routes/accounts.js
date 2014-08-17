4541e544516ec6e89ffcdce807f5d5507789bc3a
toran billups <toranb@gmail.com>
2014-08-16 20:15:40 -0500

    working accounts (non promise based)

--- js/app/routes/accounts.js
+++ js/app/routes/accounts.js
@@ -2,15 +2,14 @@ import AjaxMixin from 'js/mixins/ajax';
 
 var AccountsRoute = Ember.Route.extend(AjaxMixin, {
     model: function() {
-        //Ember.run(self.people, self.people.pushObject, thing);
-        console.warn("IN the model hook");
-        return this.xhr('/api/accounts', 'get').then(function(response) {
-            things.forEach(function(thing) {
-                console.warn(thing);
-                console.warn(thing.name);
+        var accounts = [];
+        var self = this;
+        $.getJSON('/api/accounts', function(response) {
+            response.forEach(function(account) {
+                Ember.run(accounts, accounts.pushObject, account);
             });
-            return [];
-        });
+        }, this);
+        return accounts;
     }
 });
 
--- js/tests/integration/accounts_tests.js
+++ js/tests/integration/accounts_tests.js
@@ -11,11 +11,20 @@ module('accounts integration tests', {
     }
 });
 
+test('model will populate the controller with nothing when xhr returns empty list', function() {
+    visit("/accounts")
+    .httpRespond("get", "/api/accounts", [])
+    .then(function() {
+        missing("table tr");
+    });
+});
+
 test('model will populate the controller when promise resolves with 2 accounts', function() {
-    var accounts = [{name: "x"}, {name: "y"}];
+    var accounts = [{name: "first"}, {name: "last"}];
     visit("/accounts")
     .httpRespond("get", "/api/accounts", accounts)
     .then(function() {
-        missing("table tr");
+        var rows = find("table tr").length;
+        equal(rows, 2, rows);
     });
 });
