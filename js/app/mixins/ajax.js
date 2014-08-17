a063d55acf292f951ca2bff26ea3ec139bf8c83e
toran billups <toranb@gmail.com>
2014-08-16 21:03:44 -0500

    added (now broken again) promise version

--- js/app/mixins/ajax.js
+++ js/app/mixins/ajax.js
@@ -1,31 +1,34 @@
 var AjaxMixin = Ember.Mixin.create({
     xhr: function(url, type, hash) {
-        console.warn("in the xhr");
         hash = hash || {};
         hash.url = url;
         hash.type = type;
         hash.dataType = "json";
-        console.warn(url);
-        console.warn(type);
-        console.warn(hash);
         return this.promise(url, type, hash);
     },
     promise: function(url, type, hash) {
-        console.warn("in the pro 1");
         return new Ember.RSVP.Promise(function(resolve, reject) {
-            console.warn("in the pro 2");
-            hash.success = function(json) {
-              console.warn("now doing the resolve step");
-              return Ember.run(null, resolve, json);
-            };
-            hash.error = function(json) {
-                console.warn("now doing the ERROR step");
-                if (json && json.then) {
-                    json.then = null;
-                }
-                return Ember.run(null, reject, json);
-            };
-            $.ajax(hash);
+          console.log("1");
+          if (hash.data && type !== 'GET') {
+            hash.contentType = 'application/json; charset=utf-8';
+            hash.data = JSON.stringify(hash.data);
+          }
+
+          hash.success = function(json) {
+            console.log("2");
+            Ember.run(null, resolve, json);
+          };
+
+          hash.error = function(jqXHR, textStatus, errorThrown) {
+            console.log("3");
+            if (jqXHR) {
+              jqXHR.then = null;
+            }
+
+            Ember.run(null, reject, jqXHR);
+          };
+
+          Ember.$.ajax(hash);
         });
     }
 });
--- js/app/routes/accounts.js
+++ js/app/routes/accounts.js
@@ -2,14 +2,7 @@ import AjaxMixin from 'js/mixins/ajax';
 
 var AccountsRoute = Ember.Route.extend(AjaxMixin, {
     model: function() {
-        var accounts = [];
-        var self = this;
-        $.getJSON('/api/accounts', function(response) {
-            response.forEach(function(account) {
-                Ember.run(accounts, accounts.pushObject, account);
-            });
-        }, this);
-        return accounts;
+        return this.xhr('/api/accounts', 'GET');
     }
 });
 
